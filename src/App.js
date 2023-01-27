04cd66aee5be262c9651970f23a99e2a39a8c67d
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-26 20:09:02 -0800

    Import and add ChatEntry component in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+
+  const chatentryComponents = chatMessages.map(message =>{
+    return <ChatEntry sender={message.sender} body={message.body} timeStamp={message.timeStamp}/>;
+  });
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        {chatentryComponents}
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
