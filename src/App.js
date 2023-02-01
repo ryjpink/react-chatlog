483017615da1959e053514a9158619130e52c8e2
Ya-Juan Ruan <yajuan@decltype.org>
2023-02-01 12:37:09 -0800

    Bring back getheartcount function

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
-let heartCount = 0;
 const App = () => {
   const [messages, setMessages] = useState(chatMessages);
   const owner = 'Vladimir';
   const updateLike = (updatedmessage)=>{
     const updatedmessages = messages.map(message =>{
       if (message.id === updatedmessage.id){
-        if (updatedmessage.liked){
-          heartCount ++;
-        }else{
-          heartCount --;
-        }
         return updatedmessage;
 
       }else{
@@ -24,21 +18,21 @@ const App = () => {
     setMessages(updatedmessages);
   }
 
-  // const getHeartCount = () =>{
-  //   let heartCount = 0;
-  //   for (const message of messages){
-  //     if (message.liked){
-  //       heartCount ++;
-  //     }
-  //   }
-  //   return heartCount;
-  // }
+  const getHeartCount = () =>{
+    let heartCount = 0;
+    for (const message of messages){
+      if (message.liked){
+        heartCount ++;
+      }
+    }
+    return heartCount;
+  }
 
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
-        <section><span className='widget' id='heartWidget'>{heartCount} ❤️s</span></section>
+        <section><span className='widget' id='heartWidget'>{getHeartCount()} ❤️s</span></section>
       </header>
       <main>
         <ChatLog entries={messages} onupdate={updateLike} owner={owner}></ChatLog>
