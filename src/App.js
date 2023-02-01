04e5400506dee84b6d55eb99b9b22dd3d3e0a0ed
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-31 22:22:44 -0800

    Update updatelike function and add heartcount variable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
+let heartCount = 0;
 const App = () => {
   const [messages, setMessages] = useState(chatMessages);
  
@@ -11,7 +12,13 @@ const App = () => {
   const updateLike = (updatedmessage)=>{
     const updatedmessages = messages.map(message =>{
       if (message.id === updatedmessage.id){
+        if (updatedmessage.liked){
+          heartCount ++;
+        }else{
+          heartCount --;
+        }
         return updatedmessage;
+
       }else{
         return message;
       }
@@ -19,21 +26,21 @@ const App = () => {
     setMessages(updatedmessages);
   }
 
-  const getHeartCount = () =>{
-    let heartCount = 0;
-    for (const message of messages){
-      if (message.liked){
-        heartCount ++;
-      }
-    }
-    return heartCount;
-  }
+  // const getHeartCount = () =>{
+  //   let heartCount = 0;
+  //   for (const message of messages){
+  //     if (message.liked){
+  //       heartCount ++;
+  //     }
+  //   }
+  //   return heartCount;
+  // }
 
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
-        <section><span className='widget' id='heartWidget'>{getHeartCount()} ❤️s</span></section>
+        <section><span className='widget' id='heartWidget'>{heartCount} ❤️s</span></section>
       </header>
       <main>
         <ChatLog entries={messages} onupdate={updateLike}></ChatLog>
