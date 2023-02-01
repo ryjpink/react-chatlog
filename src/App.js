10968a22781e46fed8525ddeb3beabc5a0350de6
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-31 16:55:30 -0800

    Remove heartcount state and add getheartcount function

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ import ChatLog from './components/ChatLog';
 
 const App = () => {
   const [messages, setMessages] = useState(chatMessages);
-  const [heartCount, setHeartCount] = useState(0);
+ 
 
 
   const updateLike = (updatedmessage)=>{
     const updatedmessages = messages.map(message =>{
       if (message.id === updatedmessage.id){
-        if (!updatedmessage.liked){
-          setHeartCount(heartCount - 1);
-        }else{
-          setHeartCount(heartCount + 1);
-        }
         return updatedmessage;
       }else{
         return message;
@@ -24,11 +19,21 @@ const App = () => {
     setMessages(updatedmessages);
   }
 
+  const getHeartCount = () =>{
+    let heartCount = 0;
+    for (const message of messages){
+      if (message.liked){
+        heartCount ++;
+      }
+    }
+    return heartCount;
+  }
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
-        <section><span className='widget' id='heartWidget'>{heartCount} ❤️s</span></section>
+        <section><span className='widget' id='heartWidget'>{getHeartCount()} ❤️s</span></section>
       </header>
       <main>
         <ChatLog entries={messages} onupdate={updateLike}></ChatLog>
